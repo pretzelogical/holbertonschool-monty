2b4af43e461348693fd0a7829bcf00622ec42e40
pretzelogical <yingyangyo266@gmail.com>
2023-03-29 11:26:31 -0500

    Started & added file reading

---
+++ monty.h
@@ -0,0 +1,53 @@
+#ifndef MONTY_h
+#define MONTY_h
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack	 (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct stack_s
+{
+        int n;
+        struct stack_s *prev;
+        struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct instruction_s
+{
+        char *opcode;
+        void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+/**
+ * struct file_s- file and associated data
+ * @script: pointer to currently open file
+ * @linenum: current line number (starting at 1)
+ * 
+*/
+typedef struct file_s
+{
+	FILE *script;
+	unsigned int linenum;
+
+}file_t;
+
+file_t *open_file(char *filename);
+void close_file(file_t *file);
+
+#endif
